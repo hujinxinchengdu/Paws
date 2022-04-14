352e9581529c1dbfb8cb2823dba5543bf5e6bfa3
eileenzhangyf <zhangbbq@bc.edu>
2022-04-14 16:55:14 -0700

    profile set up pages updated

---
+++ app/src/main/java/edu/neu/madcourse/paws/PetSettingActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.paws;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PetSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet_setting);
+    }
+}
--- app/src/main/java/edu/neu/madcourse/paws/settingActivity.java
+++ app/src/main/java/edu/neu/madcourse/paws/settingActivity.java
@@ -2,7 +2,10 @@ package edu.neu.madcourse.paws;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class settingActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class settingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        Button next_button = (Button) findViewById(R.id.button_next);
+        next_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPetSetting();
+            }
+        });
+    }
+
+    public void openPetSetting(){
+        Intent intent = new Intent(this,PetSettingActivity.class);
+        startActivity(intent);
     }
 }
